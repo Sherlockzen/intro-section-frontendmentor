cf4a131d8a84d53f376bbb46ee303fdd39e61f01
Hewerton <hewertonlourenco@gmail.com>
2023-04-23 13:03:11 -0300

    Init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div className=' w-screen h-screen bg-AlmostWhite'>
+      <h1 className=' bg-white text-6xl font-Epilogue'>Aqui temos algo</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
